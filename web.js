98e129124b1c2a1b0f2e8bd7f2c0e5b724d8ce96
markallanhatcher <markallanhatcher@gmail.com>
2013-08-10 00:21:40 +0000

    added updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
+
+var buffIndexFile = fs.readFileSynch('index.html');
+console.log(buffIndexFile);
+
 
 app.get('/', function(request, response) {
   response.send('Hello World2 From Mark!');
