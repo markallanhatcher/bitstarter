44e8847f5597c87992ea8bbdebee7663dd789aba
markallanhatcher <markallanhatcher@gmail.com>
2013-08-10 02:17:41 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express();
-//var fs = require('fs');
+var fs = require('fs');
 
 app.use(express.logger());
-
-//var buffIndexFile = fs.readFileSynch('./index.html');
-//console.log(buffIndexFile);
+var buffer = fs.readFileSync('index.html');
+var htmlString = buffer.toString();
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World2 From Mark!');
+  response.send(htmlString);
 });
 
 var port = process.env.PORT || 5000;
