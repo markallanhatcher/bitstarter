e82c90b0d61d4bf57c5f842172fca6431ccfd9f3
markallanhatcher <markallanhatcher@gmail.com>
2013-08-10 00:25:23 +0000

    added

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var fs = require('fs');
+//var fs = require('fs');
 
 app.use(express.logger());
 
-var buffIndexFile = fs.readFileSynch('index.html');
-console.log(buffIndexFile);
+//var buffIndexFile = fs.readFileSynch('./index.html');
+//console.log(buffIndexFile);
 
 
 app.get('/', function(request, response) {
